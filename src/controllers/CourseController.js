5d6cfa5ff4b44b1eca9dcd86bb7a7f593f66812f
johao <johao.gs10@gmail.com>
2023-12-01 11:24:22 -0500

    add controllers of courses

--- src/controllers/CourseController.js
+++ src/controllers/CourseController.js
@@ -16,6 +16,52 @@ module.exports = {
                 message: err.message || "Ocurrio un error mientras se guardaba"
             })
         })
+    },
+
+    updateCourse(req, res){
+
+        try{
+            Course.findAll({ where: { id: req.body.courseId }})
+            .then(async (result) => {
+                if( result.length > 0){
+                    Course.update({
+                        name: req.body.name
+                    },
+                    {
+                        where: { id: req.body.courseId }
+                    });
+                    res.status(200).json({
+                        message: "actualizacion correcta",
+                        name: req.body.name
+                    })
+                }else{
+                    res.status(500).json({ message: "actualizacion fallida" })
+                }
+            })
+        } catch( err) {
+            res.status(404).json({ message: err });
+        }
+    },
+
+    deleteCourse(req,res){
+        try{
+            Course.findAll({ where: { id: req.body.courseId }})
+            .then(async (result) => {
+                if( result.length > 0){
+                    Course.destroy({
+                        message: "eliminacion correcta",
+                        where: { id: req.body.courseId }
+                    });
+                    res.status(200).json({
+                        message: "curso eliminado"
+                    })
+                }else{
+                    res.status(500).json({ message: "eliminacion fallida" });
+                }
+            })
+        } catch( err) {
+            res.status(404).json({ message: err });
+        }
     }
 
 }
